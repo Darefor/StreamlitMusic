d0a70d6553b37c13347297369ec4126b70f64672
Darefor <88797296+Darefor@users.noreply.github.com>
2025-04-16 02:19:27 +0300

    .

---
+++ usedb.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from sqlalchemy import create_engine
+
+
+def use_db():
+    dbname = st.secrets["connection"]["url"]
+    # dbname = st.secrets["URL"]
+    engine = create_engine(dbname)
+    return engine
