948a5e7584623691fa604a8c0106e756a003f289
Darefor <88797296+Darefor@users.noreply.github.com>
2025-04-16 02:31:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from usedb import usedb
+from usedb import use_db
 from use_content import content
 from use_section import section
 
-engine = usedb()
+engine = use_db()
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
